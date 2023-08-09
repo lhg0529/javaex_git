3836a4ca73d3258e18a189bb9b364fc360ce4436
lhg0529 <lhg0529@naver.com>
2023-08-09 17:41:52 +0900

    v1.0-java first day

---
+++ newproject/src/sample/Hello.java
@@ -0,0 +1,23 @@
+package sample;
+
+/**
+ * Hello sample class
+ * main-method
+ * hello 출력하는 프로그램
+ */
+public class Hello {
+  public static void main(String[] args) {
+    char zero = '0';
+    char three = 3 + '0';
+    int i = '3' - '0';
+    String str3 = 3.0 + "";
+
+    double result = (double)7/4;
+    System.out.println(result);
+    System.out.println("result = " + result);
+
+//    String hello = "안녕 !";
+//    System.out.println("hello, world!");
+  }
+}
+
---
+++ newproject/src/sample/inputoutput.java
@@ -0,0 +1,17 @@
+  package sample;
+
+  import java.util.Scanner;
+
+  public class inputoutput {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int i = scanner.nextInt();
+      System.out.println(i);
+
+      System.out.printf("iii\n iii");
+
+      String s = scanner.nextLine();
+      int i2 = Integer.parseInt(s);
+      System.out.println(i2);
+    }
+}
