95cdc605f1a50618045a71773108ff52fb4b6c8d
lhg0529 <lhg0529@naver.com>
2023-08-10 17:45:41 +0900

    java day2

--- newproject/src/control/SimpleIf.java
+++ newproject/src/control/SimpleIf.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SimpleIf {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    System.out.println("숫자를 입력해주세요.");
+    System.out.println("숫자를 d입력해주세요.");
     int num = in.nextInt();
     if (num % 2 == 0) {
       System.out.println("짝수입니다");
--- newproject/src/method/MethodEx.java
+++ newproject/src/method/MethodEx.java
@@ -1,5 +1,5 @@
 package method;
-
+/* */
 public class MethodEx {
   public static void main(String[] args) {
 //    int sum = 0;
